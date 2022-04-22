486ba6ac5cecf003c8f54d5519a8f7407904079e
thaianh4780 <thaianh4780@mail.tdc.edu.vn>
2022-04-22 22:10:12 +0700

    all file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '6223dd4a31d76db65c4697e1441022e7a13290b8',
+        'name' => 'thaianh4780/mypackage',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'tailieuweb/mypackage' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../tailieuweb/mypackage',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => '071250e444f58ee406ad5a1a815d5d33323c78ac',
+            'dev_requirement' => false,
+        ),
+        'thaianh4780/mypackage' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '6223dd4a31d76db65c4697e1441022e7a13290b8',
+            'dev_requirement' => false,
+        ),
+    ),
+);
